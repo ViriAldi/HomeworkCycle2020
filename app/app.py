31e2f68389be3c5775319e745d7c4917a2d65d43
ViriAldi <v.fedynyak@ucu.edu.ua>
2020-05-15 01:26:17 +0300

    working on web page

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from mpl_3d.plotly_3d import save_csv
 
 
@@ -7,8 +7,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    save_csv((50, 5), 30000)
     return render_template("index.html")
+
+
+@app.route("/map", methods=["POST"])
+def geo_map():
+    size = request.form.get("geo")
+    if not size.isdecimal():
+        return "Bad input"
+    size = int(size)
+    if size < 1 or size > 40:
+        return "Bad value"
+
+    save_csv((50, 5), size * 1000)
+    return render_template("map.html")
 
 
 app.run(debug=True)
