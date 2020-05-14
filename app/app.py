4f2cd63d3a69f6bd00d8ad6fd72daed4e1835737
ViriAldi <v.fedynyak@ucu.edu.ua>
2020-05-15 00:32:05 +0300

    designing a plot

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    save_csv((50, 5), 2000)
+    save_csv((50, 5), 10000)
     return render_template("index.html")
 
 
--- mpl_3d/plotly_3d.py
+++ mpl_3d/plotly_3d.py
@@ -6,7 +6,7 @@ def save_csv(geo_corner, size):
     reader = GeoTiffReader("../geofiles/swizerland.tif", geo_corner)
 
     lattice = reader.make_lattice((45.976607, 7.658548), size=size)
-    x, y, z = lattice.x_2d, lattice.y_2d, lattice.z_2d
+    x, y, z = lattice.x_2d, lattice.y_2d, lattice.z_2d - 20
 
     df = pd.DataFrame(z)
     df.to_csv("../app/static/data_z.csv")
@@ -17,8 +17,9 @@ def save_csv(geo_corner, size):
     df = pd.DataFrame(y)
     df.to_csv("../app/static/data_y.csv")
 
-    # points = lattice.path((4, 2), (200, 200))
-    # x_p = list(map(lambda p: p.get_x(), points))
-    # y_p = list(map(lambda p: p.get_y(), points))
-    # z_p = list(map(lambda p: 2 * p.get_z(), points))
-
+    points = lattice.path((100, 0), (0, 100))
+    x_p = list(map(lambda p: p.get_x(), points))
+    y_p = list(map(lambda p: p.get_y(), points))
+    z_p = list(map(lambda p: p.get_z(), points))
+    df = pd.DataFrame([x_p, y_p, z_p])
+    df.to_csv("../app/static/data_path.csv")
